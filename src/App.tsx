6e9a39a204c969e2978f4acc8078d6ac727808ca
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-09-02 21:54:16 +1000

    react typescript init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title='dostoevsky.rest' color='red' />
+    </div>
+  );
+}
+
+export default App;
