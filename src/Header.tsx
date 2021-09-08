927c0332a56e092a24e372efab9c706ecd083f64
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-09-08 23:01:46 +1000

    quotes added

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,3 +1,4 @@
+import quotes from '../src/lib/quotes.json'
 export interface Props {
     title: string,
     color?: string
@@ -7,6 +8,7 @@ const Header  = (props: Props) => {
     return (
         <header>
             <h1 style={{color: props.color?props.color: 'blue'}}>{props.title}</h1>
+            <p>{JSON.stringify(quotes)}</p>
         </header>
     )
 }
