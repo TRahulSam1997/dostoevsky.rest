bfe5d9be6d041d41bb9df9d0df9c4507079808b7
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-09-13 21:46:22 +1000

    quote generator added

--- backend/quotes.ts
+++ backend/quotes.ts
@@ -5,12 +5,26 @@ export class Quotes {
 
     private app: Express;
 
+    private headers:any = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "content-type": "application/json"
+    };
+
     constructor(app: Express) {
         this.app = app;
 
-        this.app.get("/api", (req: Request, res: Response): void => {
-            res.send(quotes);
-        })
+        try {
+            this.app.get("/api", (req: Request, res: Response): void => {
+                let quote:string = quotes[Math.floor(Math.random() * quotes.length)];
+                let resQuote:JSON = JSON.parse(`{"quote": "${quote}"}`);
+                req.headers[this.headers]
+                res.send(resQuote);
+            })
+        } catch (e) {
+            console.log(e);
+        }
     }
 
     public start(port: number): void {
