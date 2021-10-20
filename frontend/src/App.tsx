3b90629474fa7288eb56ffee10da7449b7db9847
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-10-20 21:40:29 +1100

    frontend updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ const App = () => {
   return (
     <div>
       <h1>Dostoevsky.rest</h1>
+      <p>A REST API for Fyodor Dostoevsky quotes</p>
       <button onClick={refetch}>Quote</button>
       <div>{!quote && loading? "loading..." : quote.data}</div>
     </div>
