daa5a21c35567e6035feca1e8b53e13828acb9e6
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-10-21 21:40:22 +1100

    styling added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,6 +15,14 @@ const App = () => {
       <p>A REST API for Fyodor Dostoevsky quotes</p>
       <button onClick={refetch}>Quote</button>
       <div>{!quote && loading? "loading..." : obj.quote}</div>
+      <div>
+        <p>Created by <a href="https://rahulsam.me/">Rahul</a></p>
+      </div>
+      <div>
+        <a href="https://github.com/TRahulSam1997/dostoevsky.rest">
+          <img src="https://img.shields.io/github/stars/TRahulSam1997/dostoevsky.rest?style=social" alt="Github Link"></img>
+        </a>
+      </div>
     </div>
   )
 }
