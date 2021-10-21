607af985d640262e725d391bf06acf4c7e80f661
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-10-21 21:17:10 +1100

    getting quote

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import useFetch from './utils/useFetch'
+import useFetch, {Payload} from './utils/useFetch'
 
 const App = () => {
   const { quote, refetch, loading } = useFetch('/api/rand');
@@ -8,12 +8,13 @@ const App = () => {
     refetch();
   }, []);
 
+  let obj = JSON.parse(!quote.data? '{}' : quote.data);
   return (
     <div>
       <h1>Dostoevsky.rest</h1>
       <p>A REST API for Fyodor Dostoevsky quotes</p>
       <button onClick={refetch}>Quote</button>
-      <div>{!quote && loading? "loading..." : quote.data}</div>
+      <div>{!quote && loading? "loading..." : obj.quote}</div>
     </div>
   )
 }
